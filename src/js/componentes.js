86ffe2a9135854b9a05a50de0ea97c92f8eba359
ALEX GALLARDO IBACETA <ALEX.IBACETA@gmail.com>
2022-03-22 18:20:54 -0300

    componentes.js:const crearTodoHtml, txtInput referencia a .new-todo, evento Keyup

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,18 +1,25 @@
-import '../css/componentes.css';
-// import webpacklogo from '../assets/img/webpack-logo.png';
+// referencias en el html
+const divTodoList = document.body.querySelector('.todo-list');
+const txtInput = document.body.querySelector('.new-todo');
 
 
-export const saludar = ( nombre = 'sin nombre' ) => {
-    console.log('Creando etiqueta h1');
+export const crearTodoHtml = (todo) => {
+    const htmlTdo = `<li class="${(todo.completado)?'completed':''}" data-id="${todo.id}">
+    <div class="view">
+    <input class="toggle" type="checkbox" ${(todo.completado)?'checked':''}>
+    <label>${todo.tarea}</label>
+    <button class="destroy"></button>
+    </div>
+    <input class="edit" value="Create a TodoMVC template">
+    </li>`;
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola ${ nombre }`;
-
-    document.body.append( h1 );
-
-    
-    // Img
-    // const img = document.createElement('img');
-    // img.src = webpacklogo;
-    // document.body.append( img );
+    const div = document.createElement('div');
+    div.innerHTML = htmlTdo;
+    divTodoList.append(div.firstElementChild);
+    return div.firstElementChild;
 }
+
+// Eventos
+txtInput.addEventListener('keyup', (event) => {
+    console.log(event);
+});
